81c00f270612c8f51fafbde26a69e76e10209907
Molnár Ferenc <fezsa97@gmail.com>
2018-12-02 22:42:24 +0100

    db change

--- Recipe/db/schema.rb
+++ Recipe/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_12_01_171236) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email", default: "", null: false
