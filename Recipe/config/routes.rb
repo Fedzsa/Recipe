ad23cf4307fe92523746f19c179288aacb211891
Ferenc Molnár <fezsa97@gmail.com>
2018-12-09 16:55:39 +0100

    create user and new recipe view

--- Recipe/config/routes.rb
+++ Recipe/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'devise/registrations', sessions: 'devise/sessions' }
   root 'home#index'
-
+  get 'user/:id', to: 'user#index'
+  get 'user/:id/create', to: 'user#create'
 end
