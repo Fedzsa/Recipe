1c09cf26563cf6fee0dacf5e70616f6cbe4e6c50
Ferenc Molnár <fezsa97@gmail.com>
2018-12-09 14:18:22 +0100

    fix registration

--- Recipe/config/routes.rb
+++ Recipe/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'devise/registrations', sessions: 'devise/sessions' }
   root 'home#index'
-  
+
 end
