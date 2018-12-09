80fd3d52652f20020677347fd9199a066a7ce42f
Ferenc Molnár <fezsa97@gmail.com>
2018-12-09 15:24:17 +0100

    change

--- Recipe/config/environments/production.rb
+++ Recipe/config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
   config.assets.initialize_on_precompile = true
   # config.assets.css_compressor = :sass
 
